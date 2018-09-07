5bdcb170d2b4df336c7d5c15e5414e116f075c28
Eric Shull <eshulldesign@gmail.com>
2018-09-07 11:28:31 -0700

    add seed file with info. Not working yet

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,27 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+Product.destroy_all
+Review.destroy_all
+
+30.times do |index|
+  Product.create!(name: Faker::Food.fruits,
+    cost: Faker::Number.non_zero_digit,
+    origin: "USA")
+end
+
+30.times do |index|
+  Product.create!(name: Faker::Food.fruits,
+    cost: Faker::Number.non_zero_digit,
+    origin: "Mexico")
+end
+
+Product.all.each do |product|
+    60.times do |index|
+      product.review.create!(author: Faker::Name.name, content: Faker::Lorem.sentence(20),
+      rating: Faker::Number.between(1, 5),
+      product_id: Product.all.shuffle[0].id)
+  end
+end
+
+p "Created #{Product.count} products"
