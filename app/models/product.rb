87b367a0e3ffb7196f9cdc4f9bb6f60c08f4404b
Eric Shull <eshulldesign@gmail.com>
2018-09-07 15:48:32 -0700

    add scope method and display for highest rated product

--- app/models/product.rb
+++ app/models/product.rb
@@ -3,9 +3,9 @@ class Product < ActiveRecord::Base
 
   scope :sort_recent, -> {(
   select("products.id, products.name, products.cost, products.origin")
-  .order("(products.created_at) ASC")
+  .order("(products.created_at) DESC")
   )}
-  
+
   scope :most_ratings, -> {(
   select("products.id, products.name, products.cost, products.origin, count(reviews.id) as reviews_count")
     .joins(:reviews)
@@ -13,4 +13,12 @@ class Product < ActiveRecord::Base
     .order("reviews_count DESC")
     .limit(1)
   )}
+
+  scope(:highest_rating, ->do
+      select("products.id, products.name, products.origin, products.cost, count(reviews.rating) as reviews_rating")
+      .joins(:reviews)
+      .group("products.id")
+      .order("reviews_rating DESC")
+      .limit(1)
+    end)
 end
--- app/models/review.rb
+++ app/models/review.rb
@@ -2,10 +2,6 @@ class Review < ActiveRecord::Base
   belongs_to :product
   validates :author, :presence => true
 
-  scope(:highest_rating, ->do
-      select("products.id, products.name, products.origin, products.reviews.rating")
-      where({:rating => 5})
-      .limit(1)
-    end)
+
 
 end
