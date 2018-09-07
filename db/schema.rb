1ad2bff970ebc1eaa18bb07eee0bd6c74f980c5f
Eric Shull <eshulldesign@gmail.com>
2018-09-07 09:46:38 -0700

    add second migration to add reviews table

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_09_07_163313) do
+ActiveRecord::Schema.define(version: 2018_09_07_164348) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -19,6 +19,15 @@ ActiveRecord::Schema.define(version: 2018_09_07_163313) do
     t.string "name"
     t.integer "cost"
     t.string "origin"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "reviews", force: :cascade do |t|
+    t.string "author"
+    t.string "content"
+    t.integer "rating"
+    t.integer "product_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
