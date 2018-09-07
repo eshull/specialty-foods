847c8a27508f13212088cedd3c2a27d330fdc38b
Eric Shull <eshulldesign@gmail.com>
2018-09-07 14:42:05 -0700

    successful display of scope method for returning product with most reviews

--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,16 @@
 class Product < ActiveRecord::Base
   has_many :reviews
+
+  scope :sort_recent, -> {(
+  select("products.id, products.name, products.origin")
+  .order("lower(products.created_at) ASC")
+  )}
+
+  scope :most_ratings, -> {(
+  select("products.id, products.name, products.cost, products.origin, count(reviews.id) as reviews_count")
+    .joins(:reviews)
+    .group("products.id")
+    .order("reviews_count DESC")
+    .limit(1)
+  )}
 end
