bb99d2b3cdb686a82dc2df935057e37d1d50bcb9
Eric Shull <eshulldesign@gmail.com>
2018-09-07 17:14:35 -0700

    positive fail for validating content on review

--- spec/models/review_spec.rb
+++ spec/models/review_spec.rb
@@ -2,5 +2,7 @@ require 'rails_helper'
 
 describe Review do
   it { should validate_presence_of :author }
+  it { should validate_presence_of :content }
+  # it { should validate_presence_of :author }
   it { should belong_to :product }
 end
