fe85428437e90ac987d51c3270cb712d4ea695da
Eric Shull <eshulldesign@gmail.com>
2018-09-18 22:53:52 -0700

    add flash messages for creation/destruction of products/reviews

--- spec/features/new_product_pages_spec.rb
+++ spec/features/new_product_pages_spec.rb
@@ -1,14 +1,6 @@
 require 'rails_helper'
 
 describe "add a new product" do
-#   it "adds and saves new product" do
-#     visit products_path
-#     click_link 'Add New Product'
-#     fill_in 'Name', :with => 'Dragonfruit'
-#     click_on 'Create Product'
-#     expect(page).to have_content 'Products'
-#   end
-#
   it "gives error when no name is entered" do
     visit new_product_path
     click_on 'Create Product'
